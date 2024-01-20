d78c5cfb0f425192a23d22199cb1c36612e7d001
niv-e <niveliyahu25@gmail.com>
2024-01-20 12:02:57 +0200

    Create Onion architecture project structure. Add user entity

--- ReactiveUsersMicroservice/src/main/java/Cloud2024/ReactiveUsersMicroservice/ReactiveUsersMicroservice.java
+++ ReactiveUsersMicroservice/src/main/java/Cloud2024/ReactiveUsersMicroservice/ReactiveUsersMicroservice.java
@@ -1,4 +1,4 @@
-package com.example;
+package Cloud2024.ReactiveUsersMicroservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- ReactiveUsersMicroservice/src/test/java/Cloud2024/ReactiveUsersMicroservice/Cloud2024ApplicationTests.java
+++ ReactiveUsersMicroservice/src/test/java/Cloud2024/ReactiveUsersMicroservice/Cloud2024ApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example;
+package Cloud2024.ReactiveUsersMicroservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
