0b4b2e8e77035a28689caae31d02bca8f54aad58
niv-e <niveliyahu25@gmail.com>
2024-01-20 22:44:29 +0200

    remove comments

--- ReactiveUsersMicroservice/src/main/java/Cloud2024/ReactiveUsersMicroservice/ReactiveUsersMicroservice.java
+++ ReactiveUsersMicroservice/src/main/java/Cloud2024/ReactiveUsersMicroservice/ReactiveUsersMicroservice.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-//@ComponentScan(basePackages = "Cloud2024.ReactiveUsersMicroservice")
 @SpringBootApplication
-//@Configuration
 public class ReactiveUsersMicroservice {
 
 	public static void main(String[] args) {
