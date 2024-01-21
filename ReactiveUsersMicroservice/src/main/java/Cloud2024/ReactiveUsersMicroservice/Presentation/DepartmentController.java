0d78d07e5ff589ff9eb175ff80bdf7b4df47451b
niv-e <niveliyahu25@gmail.com>
2024-01-21 10:04:43 +0200

    Fix Swagger UI

--- ReactiveUsersMicroservice/src/main/java/Cloud2024/ReactiveUsersMicroservice/Presentation/DepartmentController.java
+++ ReactiveUsersMicroservice/src/main/java/Cloud2024/ReactiveUsersMicroservice/Presentation/DepartmentController.java
@@ -1,4 +1,9 @@
 package Cloud2024.ReactiveUsersMicroservice.Presentation;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = {"/department"})
 public class DepartmentController {
 }
