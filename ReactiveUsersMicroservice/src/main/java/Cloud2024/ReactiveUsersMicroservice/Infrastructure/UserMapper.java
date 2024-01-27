6d15856e938bc7dac3ec677a9e00099c0f3b1e12
niv-e <niveliyahu25@gmail.com>
2024-01-27 21:36:40 +0200

    encrypt user password when converting to user boundary

--- ReactiveUsersMicroservice/src/main/java/Cloud2024/ReactiveUsersMicroservice/Infrastructure/UserMapper.java
+++ ReactiveUsersMicroservice/src/main/java/Cloud2024/ReactiveUsersMicroservice/Infrastructure/UserMapper.java
@@ -1,13 +1,36 @@
 package Cloud2024.ReactiveUsersMicroservice.Infrastructure;
 
+import Cloud2024.ReactiveUsersMicroservice.Domain.UserEntity;
+import Cloud2024.ReactiveUsersMicroservice.Presentation.Boundaries.UserBoundary;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.PropertyMap;
 import org.springframework.stereotype.Component;
+
+import java.util.Random;
 
 @Component
 public class UserMapper {
 
     public ModelMapper modelMapper(){
-        return new ModelMapper();
+        var userMapper =  new ModelMapper();
+        var userMap = new PropertyMap<UserEntity, UserBoundary>() {
+            protected void configure() {
+                map().setPassword(generateRandomAsterisks());
+            }
+        };
+
+        userMapper.addMappings(userMap);
+        return userMapper;
     }
 
+
+    private String generateRandomAsterisks() {
+        Random random = new Random();
+        int num = random.nextInt(13) + 8; // Generate a random number between 8 and 20
+        StringBuilder sb = new StringBuilder(num);
+        for (int i = 0; i < num; i++) {
+            sb.append('*');
+        }
+        return sb.toString();
+    }
 }
